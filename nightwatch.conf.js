7cd20212c82788e3aa306fc29ba8904f7c1d19c3
Dominic Foti. Jr <dominic@fotijr.com>
2017-07-07 14:06:37 -0400

    Travis saucelab debugging

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -2,16 +2,16 @@
 var travisJob = (typeof TRAVIS_JOB_NUMBER !== "undefined") ? TRAVIS_JOB_NUMBER : "";
 
 module.exports = {
-  'src_folders': [
-    'integration'
+  "src_folders": [
+    "integration"
   ],
-  'output_folder': 'reports',
-  'custom_commands_path': '',
-  'custom_assertions_path': '',
-  'page_objects_path': '',
+  "output_folder": "reports",
+  "custom_commands_path": "",
+  "custom_assertions_path": "",
+  "page_objects_path": "",
   "globals_path": "integration/nightwatch-globals.js",
-  'selenium': {
-    'start_process': false
+  "selenium": {
+    "start_process": false
   },
   "test_settings": {
     "default": {
@@ -28,11 +28,22 @@ module.exports = {
       }
     },
     "ci": {
-      "launch_url": 'http://ondemand.saucelabs.com:80',
+      "launch_url": "http://ondemand.saucelabs.com:80",
       "desiredCapabilities": {
         "build": `build-${travisJob}`,
-        'tunnel-identifier': travisJob,
-      },
+        "tunnel-identifier": travisJob,
+      }
+    },
+    "localToSauce": {
+      "launch_url": "http://localhost",
+      "selenium_port": 80,
+      "selenium_host": "ondemand.saucelabs.com",
+      "username": "FotiJr",
+      "access_key": "7f2e3b7f-d3ef-4e7d-88e8-369e306d3e07",
+      "desiredCapabilities": {
+          "javascriptEnabled": true,
+          "browserName": "chrome"
+      }
     }
   }
 };
